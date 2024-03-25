e9ad625f5c68cbdc516db1f3bb02a7a149cb3fc9
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-03-25 13:47:08 +0900

    Title: 두 사람, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/두 사람/two-person.py
+++ 240325/두 사람/two-person.py
@@ -1,17 +1,8 @@
-info = input()
-info2 = input()
+[age_a,gender_a]=input().split()
+[age_b,gender_b]=input().split()
 
-arr = info.split()
-arr2 = info2.split()
+if (int(age_a) > 18 and gender_a=='M') or (int(age_b)>18 and gender_b=='M'):
+    print(1)
 
-age = int(arr[0])
-gender = (arr[1])
-
-age2 = int(arr2[0])
-gender2 = (arr2[1])
-
-if (age >= 19 or age2 >= 19) :
-    if gender == "M" or gender2 == "M" :
-        print(1)
 else :
     print(0)
