649ef6bf4e41f8e52ad3bab5e2d2bac1181c9180
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-03-25 13:45:59 +0900

    Title: 두 사람, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240325/두 사람/two-person.py
+++ 240325/두 사람/two-person.py
@@ -10,8 +10,8 @@ gender = (arr[1])
 age2 = int(arr2[0])
 gender2 = (arr2[1])
 
-if age >= 19 or age2 >= 19:
-    if gender == "M" :
+if (age >= 19 or age2 >= 19) :
+    if gender == "M" or gender2 == "M" :
         print(1)
 else :
     print(0)
