af88942b0b1f63935b255b83d4fe5fd150c27abd
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-03-26 17:36:18 +0900

    Title: a/b 출력, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240326/a/b 출력/a-divide-b.py
@@ -0,0 +1,16 @@
+a,b = map(int, input().split())
+
+i = a//b
+print(f'{i}.', end = '')
+
+for _ in range(20):
+    if i * b == a:
+        print(0, end = '')
+    elif i == 0:
+        a *= 10
+        print(a//b, end = '')
+        i = a//b
+    else:
+        a = (a - i * b)*10
+        print(a//b, end= '')
+        i = a//b
