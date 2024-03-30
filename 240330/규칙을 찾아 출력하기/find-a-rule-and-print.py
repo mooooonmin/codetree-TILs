d9939452d9213ae36a03597e02314037edb5d67b
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-03-30 19:50:18 +0900

    Title: 규칙을 찾아 출력하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240330/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240330/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -1,13 +1,16 @@
-def print_stars(n):
-    for i in range(n):
-        if i == 0 or i == n - 1:
-            print("* " * n)
-        else:
-            print("* " + "  " * (n - 2) + "*")
-
 n = int(input())
 
-if 1 <= n <= 10:
-    print_stars(n)
-else:
-    print("..")
+for i in range(n):
+    if i == 0:
+        for _ in range(n):
+            print("*", end=" ")
+        print()
+    else:
+        for j in range(n):
+            if j == (n - 1):
+                print("*", end="")
+            elif j <= i - 1:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
