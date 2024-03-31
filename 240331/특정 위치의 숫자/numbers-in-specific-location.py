d68e825adb62227d75ced59cc5890a3e4be5cb39
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-03-31 13:44:55 +0900

    Title: 특정 위치의 숫자, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240331/특정 위치의 숫자/numbers-in-specific-location.py
@@ -0,0 +1,6 @@
+numbers = list(map(int, input().split()))  # 10개의 정수 입력받기
+
+# 세 번째, 다섯 번째, 열 번째 정수의 합 계산
+sum_of_selected_numbers = numbers[2] + numbers[4] + numbers[9]
+
+print(sum_of_selected_numbers)  # 합 출력
