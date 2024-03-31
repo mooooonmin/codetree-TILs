5033b6b0b403da7ebd3bf798af5fe7bee68e506f
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-03-31 13:33:31 +0900

    Title: 10개 입력받기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240331/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,14 @@
+numbers = list(map(int, input().split()))
+
+total = 0
+count = 0
+
+for num in numbers:
+    if num == 0:
+        break
+    total += num
+    count += 1
+
+average = total / count if count > 0 else 0
+
+print(total, round(average, 1))
