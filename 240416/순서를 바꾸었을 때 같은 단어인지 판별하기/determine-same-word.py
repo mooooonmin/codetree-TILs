ae7e7e8a5788f32d44f9f3d7d025d2a4b43f8c82
Moon's <118891828+mooooonmin@users.noreply.github.com>
2024-04-16 17:12:31 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 217ms, Memory: 29MB, Status: Passed - CodeTree

--- 240416/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
+++ 240416/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -1,20 +1,5 @@
-from collections import Counter
+str1 = input()
+str2 = input()
 
-def can_rearrange(word1, word2):
-    # 두 단어의 문자들이 동일한지 확인
-    if Counter(word1) != Counter(word2):
-        return "No"
-    
-    # 두 단어의 문자들의 순서가 동일한지 확인
-    sorted_word1 = sorted(word1)
-    sorted_word2 = sorted(word2)
-    if sorted_word1 == sorted_word2:
-        return "Yes"
-    else:
-        return "No"
-
-# 두 단어를 입력받고 함수 호출
-word1 = input()
-word2 = input()
-result = can_rearrange(word1, word2)
-print(result)
+# 각 문자열을 정렬했을 때 두 문자열이 일치하는지 비교합니다.
+print("Yes" if sorted(str1) == sorted(str2) else "No")
